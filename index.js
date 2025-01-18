9d76d1d547da9477310812d7dada0cac5707e373
ahmadsofuwan <sofuwan62@gmail.com>
2025-01-18 23:35:38 +0700

    a

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const proxySettings = JSON.parse(fs.readFileSync('./proxySettings.json'));
+process.env.GLOBAL_AGENT_HTTP_PROXY = proxySettings.httpProxy;
+process.env.GLOBAL_AGENT_HTTPS_PROXY = proxySettings.httpsProxy;
+
+require('global-agent/bootstrap');
+
 const https = require('https');
-https.get('https://ipinfo.io',  (res) => {
+
+https.get('https://ipinfo.io/json', (res) => {
   console.log(res.statusCode, res.headers);
   res.pipe(process.stdout);
+}).on('error', (err) => {
+  console.error('Error:', err);
 });
